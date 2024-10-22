30d56b1224b4a3499861c849138679335e654681
cahrl <18103864@usc.edu.ph>
2024-10-22 20:15:01 +0800

    first commit

---
+++ js/commands.js
@@ -0,0 +1,76 @@
+// var youtube = "https://www.youtube.com/fknight/";
+var twitter = "https://www.twitter.com/";
+var pumpfun = "https://pump.fun/board"
+var dexscreener = "https://dexscreener.com/"
+var password = "solanamaster1000";
+// var linkedin = "https://www.linkedin.com/in/forrestpknight/";
+// var instagram = "https://www.instagram.com/forrestpknight/";
+// var github = "https://github.com/forrestknight/";
+// var email = 'mailto:forrest@fkcodes.com';
+
+whatis = [
+  "<br>",
+  "Welcome to Sol Terminal!",
+  "Step into a realm where answers create more questions and certainty is a fleeting illusion.",
+  "Here, typing a command may lead you to wisdom—or a delightful distraction. Both, perhaps? ",
+  "Is this the beginning of your journey or the end of your sanity? Maybe it's both! ",
+  "In this space, the more you know, the less you understand, and that’s perfectly normal.",
+  "So go ahead, embrace the paradox! Every command is a path to enlightenment or confusion—take your pick.",
+  "Welcome to the terminal where everything is possible, yet nothing makes sense!",
+  "<br>"
+]
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  // 'youtube        <a href="' + youtube + '" target="_blank">youtube/fknight' + "</a>",
+  'twitter          <a href="' + twitter + '" target="_blank">twitter/' + '</a>',
+  'pumpfun          <a href="' + pumpfun + '" target="_blank">pumpfun/' + "</a>",
+  'dexscreener      <a href="' + dexscreener + '" target="_blank">dexscreener/' + '</a>',
+  // 'github         <a href="' + github + '" target="_blank">github/forrestknight' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+// projects = [
+//   "<br>",
+//   "Still curating... most projects are offline, on GitHub, or confidential.",
+//   "<br>"
+// ];
+
+help = [
+  "<br>",
+  '<span class="command">whatis</span>         What is Sol Terminal?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">SOLTERMINAL ($SOL). All RIGHTS RESERVED 2024.</span>',
+  "    _____   ____     __          ______    ______    ____     __  ___    ____    _   __    ___     __  ",
+  "   / ___/  / __ \\   / /         /_  __/   / ____/   / __ \\   /  |/  /   /  _/   / | / /   /   |   / /  ",
+  "   \\__ \\  / / / /  / /           / /     / __/     / /_/ /  / /|_/ /    / /    /  |/ /   / /| |  / /   ",
+  "  ___/ / / /_/ /  / /___        / /     / /___    / _, _/  / /  / /   _/ /    / /|  /   / ___ | / /___ ",
+  " / ____/  \\____/  /_____/       /_/     /_____/   /_/ |_|  /_/  /_/   /___/   /_/ |_/   /_/  |_|/_____/ ",
+  "                                                                                                      ",
+  '<span class="color2">Welcome to the SOL TERMINAL.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+]
+
+
